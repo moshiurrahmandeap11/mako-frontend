c48a69b4c3a8d4f95283cbc9dd25797ee08de1af
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-08-24 22:48:06 +0600

    style(page): standardize import statements and update text color for consistency

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -1,14 +1,14 @@
-'use client';
+"use client";
 
-import HeroSection from '@/components/home/HeroSection';
-import FeaturesSection from '@/components/home/FeaturesSection';
-import TechStackSection from '@/components/home/TechStackSection';
-import PricingSection from '@/components/home/PricingSection';
-import FooterSection from '@/components/home/FooterSection';
+import FeaturesSection from "@/components/home/FeaturesSection";
+import FooterSection from "@/components/home/FooterSection";
+import HeroSection from "@/components/home/HeroSection";
+import PricingSection from "@/components/home/PricingSection";
+import TechStackSection from "@/components/home/TechStackSection";
 
 export default function LandingPage() {
   return (
-    <div className="min-h-screen bg-white text-[#222325] flex flex-col overflow-x-hidden">
+    <div className="min-h-screen bg-white text-text-main flex flex-col overflow-x-hidden">
       <HeroSection />
       <FeaturesSection />
       <TechStackSection />
