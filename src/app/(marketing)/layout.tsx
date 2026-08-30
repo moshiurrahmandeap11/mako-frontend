e5b5e43c12a612bbf22dc1ecf18cf548b9c43be3
moshiurrahmandeap11 <moshiurrahmandeap@gmail.com>
2026-08-30 18:25:52 +0600

    refactor(layout): remove SplashIntro component for cleaner marketing layout

--- src/app/(marketing)/layout.tsx
+++ src/app/(marketing)/layout.tsx
@@ -1,5 +1,4 @@
 import Navbar from "@/components/Navbar";
-import SplashIntro from "@/components/SplashIntro";
 import CtaSection from "@/components/home/CtaSection";
 import FooterSection from "@/components/home/FooterSection";
 
@@ -10,7 +9,6 @@ export default function MarketingLayout({
 }) {
   return (
     <div className="min-h-screen flex flex-col bg-white">
-      <SplashIntro />
       <Navbar />
       <div className="flex-1 flex flex-col">{children}</div>
       <CtaSection />
